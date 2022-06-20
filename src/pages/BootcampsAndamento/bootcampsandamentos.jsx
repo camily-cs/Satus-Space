3db469db7dd2b13617c2b7225d3aa6b537fa5c72
Sara Morais <saramorais024@gmail.com>
2022-06-20 00:00:39 -0300

    Bootcamps em Andamento
    página concluída

--- src/pages/BootcampsAndamento/bootcampsandamentos.jsx
+++ src/pages/BootcampsAndamento/bootcampsandamentos.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import SearchBar from "../../components/SearchBar/index";
+import { AiOutlineLeft } from "react-icons/ai"
 
 import {FiPlay} from "react-icons/fi"
 
@@ -7,45 +9,82 @@ import "./style.css";
 function BootcampsAndamento() {
     return (
         <>
-            <div className="container">
-                <div className="row g-5">
+            <SearchBar/>
+            <div className="container text-white">
 
-                    <div className="col-md-4 b">
+            <div className="d-flex my-5">
+                    <AiOutlineLeft className="my-auto"/>
+                    <h2 className="ms-3">Bootcamps em Andamento</h2>
+                </div>
+
+                <div className="row g-5">
+            
+                    <div className="col-md-4 ">
                         <div>
-                            <img src="https://i.ytimg.com/vi/Ejkb_YpuHWs/maxresdefault.jpg?v=5f8061fa" className="img-fluid" alt="" />
-                            <div className="footer-video">
-                                <FiPlay/>
+                           <img src="https://i.ytimg.com/vi/Ejkb_YpuHWs/maxresdefault.jpg?v=5f8061fa" className="img-fluid" alt="" />
+                            <div className="d-flex align-items-center justify-content-between footer-video p-3">
+                                <div>
+                                <p>Introdução ao Curso</p>
+                                <p>Empresa X</p>
+                                </div>
+                                <FiPlay className=""/>
                             </div>
                         </div>
                     </div>
-
-                    <div className="col-md-4 b">
+                    <div className="col-md-4 ">
                         <div>
-                            <img src="https://img.freepik.com/fotos-gratis/paisagem-natural-em-um-jardim-botanico_35355-5948.jpg" className="img-fluid" alt="" />
+                           <img src="https://i.ytimg.com/vi/jgQjeqGRdgA/maxresdefault.jpg" className="img-fluid" alt="" />
+                            <div className="d-flex align-items-center justify-content-between footer-video p-3">
+                                <div>
+                                <p>Introdução ao Curso</p>
+                                <p>Empresa X</p>
+                                </div>
+                                <FiPlay className=""/>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="col-md-4 b">
+                    </div><div className="col-md-4 ">
                         <div>
-                            <img src="https://img.freepik.com/fotos-gratis/paisagem-natural-em-um-jardim-botanico_35355-5948.jpg" className="img-fluid" alt="" />
+                           <img src="https://i.ytimg.com/vi/xS2D9x8odoE/maxresdefault.jpg" className="img-fluid" alt="" />
+                            <div className="d-flex align-items-center justify-content-between footer-video p-3">
+                                <div>
+                                <p>Introdução ao Curso</p>
+                                <p>Empresa X</p>
+                                </div>
+                                <FiPlay className=""/>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="col-md-4 b">
+                    </div><div className="col-md-4 ">
                         <div>
-                            <img src="https://img.freepik.com/fotos-gratis/paisagem-natural-em-um-jardim-botanico_35355-5948.jpg" className="img-fluid" alt="" />
+                           <img src="https://images.passeidireto.com/thumbnails/video/93566925/thumb.jpg.large" className="img-fluid" alt="" />
+                            <div className="d-flex align-items-center justify-content-between footer-video p-3">
+                                <div>
+                                <p>Introdução ao Curso</p>
+                                <p>Empresa X</p>
+                                </div>
+                                <FiPlay className=""/>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="col-md-4 b">
+                    </div><div className="col-md-4 ">
                         <div>
-                            <img src="https://img.freepik.com/fotos-gratis/paisagem-natural-em-um-jardim-botanico_35355-5948.jpg" className="img-fluid" alt="" />
+                           <img src="https://i.ytimg.com/vi/I_vi2q6sC1k/maxresdefault.jpg" className="img-fluid" alt="" />
+                            <div className="d-flex align-items-center justify-content-between footer-video p-3">
+                                <div>
+                                <p>Introdução ao Curso</p>
+                                <p>Empresa X</p>
+                                </div>
+                                <FiPlay className=""/>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="col-md-4 b">
+                    </div><div className="col-md-4 ">
                         <div>
-                            <img src="https://img.freepik.com/fotos-gratis/paisagem-natural-em-um-jardim-botanico_35355-5948.jpg" className="img-fluid" alt="" />
+                           <img src="https://i.ytimg.com/vi/reFQrqxOzsg/maxresdefault.jpg" className="img-fluid" alt="" />
+                            <div className="d-flex align-items-center justify-content-between footer-video p-3">
+                                <div>
+                                <p>Introdução ao Curso</p>
+                                <p>Empresa X</p>
+                                </div>
+                                <FiPlay className=""/>
+                            </div>
                         </div>
                     </div>
 
