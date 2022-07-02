2312f6be4fbd016afc3757b685c3cb0b246e7078
Camily Cruz <camilycruz2003@gmail.com>
2022-07-02 18:41:57 -0300

    Alteracao na tela vagas

--- src/components/BootcampCard/index.jsx
+++ src/components/BootcampCard/index.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "./style.css";
 
 function BootcampCard({curso , concluido , empresa , vaga , prof, button, styleComponent}) {
     return (
         <>
-            <div className="row d-flex justify-content-center" style={styleComponent}>
+            <div className="row d-flex justify-content-center bootcamp" style={styleComponent}>
 
-                <div className="col-lg-5 col-md-5 p-2 py-4 bootcamp">
+                <div className="col-lg-5 col-md-5 p-2 py-4 ">
                     <img src="https://i.ytimg.com/vi/Ejkb_YpuHWs/maxresdefault.jpg?v=5f8061fa" className="img-fluid" alt="" srcset="" />
                 </div>
 
-                <div className="d-flex justify-content-center align-items-center  col-lg-7 col-md-7 py-4 bootcamp">
+                <div className="d-flex justify-content-center align-items-center  col-lg-7 col-md-7 py-4 ">
                     <div>
                         <h4>{curso}</h4>
                         <p>{concluido}%</p>
--- src/pages/Vagas/vagas.jsx
+++ src/pages/Vagas/vagas.jsx
@@ -6,22 +6,60 @@ import { BsSearch } from "react-icons/bs";
 import { MdRoom } from "react-icons/md";
 import { MdOutlinePaid } from "react-icons/md";
 import NavBarUser from "../../components/Header/NavBarUser";
-import { BootcampBox } from "../../components/BootcampBox";
-
-
+import BootcampCard from "../../components/BootcampCard";
+import { useState } from "react";
+import {BsFillMoonFill} from 'react-icons/bs';
+import {BsSun} from 'react-icons/bs';
+import { Link } from "react-router-dom";
 
 function Vagas() {
+  const [bootcampDados] = useState([
+    {
+      curso: "Curso de HTML e CSS",
+      concluido: 55,
+      empresa: "Satus Space",
+      vaga: "desenvolvedor web",
+      prof: "Gustavo Guanabara",
+      button: "Continuar",
+    },
+  ]);
+
+  /*inicio light e dark mode */
+  const [escuro, setEscuro] = useState(false)
+
+  const temaBg = {
+      backgroundColor: escuro ? "var(--cor50)" : "var(--bgcolor3)",
+      color: escuro ? "white" : "black"
+  }
+
+  const extraButton = escuro ? <Link to="#" onClick={mudarTema}> <BsFillMoonFill className="mx-2" /> Alterar Tema</Link> : <Link to="#" onClick={mudarTema}>< BsSun className="mx-2" /> Alterar Tema</Link>
+
+
+  const temaCard = {
+      backgroundColor: escuro ? "var(--bgcolor)" : "var(--bgcolor4)",
+      color: escuro ? "white" : "black"
+  }
+
+  function mudarTema() {
+      setEscuro(!escuro)
+  }
+/*fim light e dark mode */
+
+
   return (
-    <div>
-      <NavBarUser/>
+    
+    <div className="App" style={temaBg}>
+      <NavBarUser />
 
       <div className="container-fluid">
         <div className="row">
-          <Sidebar />
-          <div className="col-lg-10 col-md-12  vagas-main text-white">
+          <Sidebar 
+            extraButton={extraButton}
+          />
+          <div className="col-lg-10 col-md-12  vagas-main " style={temaCard}>
             <div className="card-header">
               <h3>Bootcamps</h3>
-              {/* <div className="inputs">
+              <div className="inputs">
                 <div className="input">
                   <div className="svgBackground">
                     <BsSearch />
@@ -79,10 +117,25 @@ function Vagas() {
                   </select>
                 </div>
                 <button>Pesquisar</button>
-              </div> */}
+              </div> 
             </div>
             <div className="card-body">
-              <BootcampBox />
+
+              {bootcampDados.map((bootcamp) => (
+                <BootcampCard
+                  /* dark e light mode */
+                  styleComponent={temaCard}
+
+                  /* dados do curso */
+                  curso={bootcamp.curso}
+                  concluido={bootcamp.concluido}
+                  empresa={bootcamp.empresa}
+                  vaga={bootcamp.vaga}
+                  prof={bootcamp.prof}
+                  button={bootcamp.button}
+                />
+              ))}
+
             </div>
           </div>
         </div>
